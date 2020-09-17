d7d07937127b3dd7fdf75e3f7e74a2498ff20a61
DiegoDuarteGuerrero <duarte.issc@gmail.com>
2020-09-17 05:23:06 -0500

    commit message

--- js/servicios.js
+++ js/servicios.js
@@ -25,12 +25,23 @@ function obtenertodosservicios() {
     querySnapshot.forEach(doc => {
           const servicio = doc.data();
           const columna = `       
-          <div class="col-md-6 col-6 col-lg-2 text-center mb-2" href="#" onclick="DetalleSer('${doc.id}')">
-          <div class="site-service-item site-animate text-center" data-animate-effect="fadeIn" >
-              <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style="border-radius: 50%; margin-bottom:10px" ></a>
-              <h2> <strong>${servicio.servicio}</strong></h2>
-              </div>
-         </div>
+
+
+          <div class="wrapper btn col-md-6 col-6 col-lg-3 text-center mb-2" href="#" onclick="DetalleSer('${doc.id}')";>   
+          <div class="card radius shadowDepth1">
+          <div class="card__image">
+          <img src="images/p1.jpg"  class="img-fluid" alt="image">
+          </div>
+          <div class="card__content card__padding">         
+          <div class="card__action"><h5><div href="#" style="font-family: sans-serif; Color: #424242;""><b>${servicio.servicio}</b></div></h6></div>
+          </div>   
+          <div class="container-fluid" style="text-align:center"><div class="card__meta"> 
+          <i class="fa fa-clock-o"  style="float: right; color: #F3C715;"><font size="2" style="font-family: sans-serif;color: #424242;">16/09/2020</font></i> 
+          </div>
+          <div> <a style="float: left; font-size: 14px;font-family: sans-serif; Color: #424242;">Leon, Gto.</a></div>
+          </div>
+          </div>
+      </div>
             `;
           html += columna;
         });
@@ -52,12 +63,22 @@ function obtenertodosservicios() {
     querySnapshot.forEach(doc => {
           const servicio = doc.data();
           const columna = `       
-          <div class="col-md-6 col-6 col-lg-2 text-center mb-2 href="#" onclick="DetalleSer('${doc.id}')">
-          <div class="site-service-item site-animate" data-animate-effect="fadeIn">
-              <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style="border-radius: 50%; margin-bottom:10px"></a>
-              <h3 class="mb-3">${servicio.servicio}</h3>
-              </div>
-         </div>
+         
+          <div class="wrapper btn col-md-6 col-6 col-lg-3 text-center mb-2" href="#" onclick="DetalleSer('${doc.id}')";>   
+          <div class="card radius shadowDepth1">
+          <div class="card__image">
+          <img src="images/p1.jpg"  class="img-fluid" alt="image">
+          </div>
+          <div class="card__content card__padding">         
+          <div class="card__action"><h5><div href="#" style="font-family: sans-serif; Color: #424242;""><b>${servicio.servicio}</b></div></h6></div>
+          </div>   
+          <div class="container-fluid" style="text-align:center"><div class="card__meta"> 
+          <i class="fa fa-clock-o"  style="float: right; color: #F3C715;"><font size="2" style="font-family: sans-serif;color: #424242;">16/09/2020</font></i> 
+          </div>
+          <div> <a style="float: left; font-size: 14px;font-family: sans-serif; Color: #424242;">Leon, Gto.</a></div>
+          </div>
+          </div>
+      </div>
             `;
           html += columna;
         });
@@ -80,12 +101,22 @@ function obtenertodosservicios() {
     querySnapshot.forEach(doc => {
           const servicio = doc.data();
           const columna = `       
-          <div class="col-md-6 col-6 col-lg-2 text-center mb-2 href="#" onclick="DetalleSer('${doc.id}')">
-          <div class="site-service-item site-animate" data-animate-effect="fadeIn">
-              <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style="border-radius: 50%; margin-bottom:10px"></a>
-              <h3 class="mb-3">${servicio.servicio}</h3>
-              </div>
-         </div>
+         
+          <div class="wrapper btn col-md-6 col-6 col-lg-3 text-center mb-2" href="#" onclick="DetalleSer('${doc.id}')";>   
+          <div class="card radius shadowDepth1">
+          <div class="card__image">
+          <img src="images/p1.jpg"  class="img-fluid" alt="image">
+          </div>
+          <div class="card__content card__padding">         
+          <div class="card__action"><h5><div href="#" style="font-family: sans-serif; Color: #424242;""><b>${servicio.servicio}</b></div></h6></div>
+          </div>   
+          <div class="container-fluid" style="text-align:center"><div class="card__meta"> 
+          <i class="fa fa-clock-o"  style="float: right; color: #F3C715;"><font size="2" style="font-family: sans-serif;color: #424242;">16/09/2020</font></i> 
+          </div>
+          <div> <a style="float: left; font-size: 14px;font-family: sans-serif; Color: #424242;">Leon, Gto.</a></div>
+          </div>
+          </div>
+      </div>
             `;
           html += columna;
         });
@@ -107,12 +138,22 @@ function obtenertodosservicios() {
     querySnapshot.forEach(doc => {
           const servicio = doc.data();
           const columna = `       
-          <div class="col-md-6 col-6 col-lg-2 text-center mb-2 href="#" onclick="DetalleSer('${doc.id}')">
-          <div class="site-service-item site-animate" data-animate-effect="fadeIn">
-              <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style="border-radius: 50%; margin-bottom:10px"></a>
-              <h3 >${servicio.servicio}</h3>
-              </div>
-         </div>
+         
+          <div class="wrapper btn col-md-6 col-6 col-lg-3 text-center mb-2" href="#" onclick="DetalleSer('${doc.id}')";>   
+          <div class="card radius shadowDepth1">
+          <div class="card__image">
+          <img src="images/p1.jpg"  class="img-fluid" alt="image">
+          </div>
+          <div class="card__content card__padding">         
+          <div class="card__action"><h5><div href="#" style="font-family: sans-serif; Color: #424242;""><b>${servicio.servicio}</b></div></h6></div>
+          </div>   
+          <div class="container-fluid" style="text-align:center"><div class="card__meta"> 
+          <i class="fa fa-clock-o"  style="float: right; color: #F3C715;"><font size="2" style="font-family: sans-serif;color: #424242;">16/09/2020</font></i> 
+          </div>
+          <div> <a style="float: left; font-size: 14px;font-family: sans-serif; Color: #424242;">Leon, Gto.</a></div>
+          </div>
+          </div>
+      </div>
             `;
           html += columna;
         });
@@ -137,12 +178,25 @@ function obtenertodosservicios() {
         if (doc.exists) {
         const servicio = doc.data();
         const columna = `
+        
+
+
         <div class="col-md-4 col-12 col-lg-4 text-center mb-2">
-        <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style=" margin-bottom:0px; margin-top:6px; margin-right:0px">
+        <div class="wrapper mb-2";>   
+        <div class="card radius shadowDepth1">
+        <div class="card__image">
+        <img src="images/p1.jpg" alt="Image placeholder" class="img-fluid" style=" margin-bottom:0px; margin-right:0px">
+        </div>
+   </div>
+   </div>
    </div>
         <div class="col-md-6 col-12 col-lg-8 mb-2">
-        <h3 >${servicio.servicio}</h3>
-        <p>${servicio.descripcion}</p>
+        <div class="wrapper mb-2";>   
+<div class="card radius shadowDepth1">
+        <div style="text-align:center; padding-top:12px;"><h4 style="color:#424242;">${servicio.servicio}</h4></div>
+        <div style="padding:12px;"><a >${servicio.descripcion}</a></div>
+        </div>
+        </div>
         </div>
        
         `;            
@@ -162,8 +216,8 @@ function obtenertodosservicios() {
                 });
                 let serviciotodosdetalle2 = document.getElementById('detalleservicio2');
 
-                let html01= `<table class="table table-hover table-bordered"> <thead><tr> <th>Descripción</th><th>Costo por hora</th><th>Costo único</th> </tr></thead><tbody>`;
-                let html02 = `  </tbody></table>`  ;
+                let html01= `<div class="container"><table class="table table-hover table-bordered" style="background-color: #fff"> <thead><tr> <th>Descripción</th><th>Costo por hora</th><th>Costo único</th> </tr></thead><tbody>`;
+                let html02 = `  </tbody></table></div>`  ;
               
            
         serviciotodosdetalle.innerHTML = html +html01+ html2+ html02;
